a354ee3e45e849a7435c6f1d8a9830679d3320c1
HermanPlay <hermanplay1110@gmail.com>
2024-12-25 15:56:31 +0100

    uncomment panic middleware

--- internal/api/http/middleware/recovery.go
+++ internal/api/http/middleware/recovery.go
@@ -1,20 +1,21 @@
 package middleware
 
 import (
+	"log"
 	"net/http"
 )
 
 // RecoveryMiddleware recovers from panics and returns a 500 error.
 func RecoveryMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// defer func() {
-		// 	if rec := recover(); rec != nil {
+		defer func() {
+			if rec := recover(); rec != nil {
 
-		// 		// TODO: Create panic logger like the one in worker to write the panic to a file
-		// 		log.Printf("Panic recovered: %v", rec)
-		// 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-		// 	}
-		// }()
+				// TODO: Create panic logger like the one in worker to write the panic to a file
+				log.Printf("Panic recovered: %v", rec)
+				http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			}
+		}()
 
 		// Call the next handler in the chain
 		next.ServeHTTP(w, r)
