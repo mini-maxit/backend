3383ac7024af3ed2fbe4b10409dfa78fbdcb7c53
HermanPlay <hermanplay1110@gmail.com>
2025-04-27 12:25:39 +0200

    feat: get, put task limits

--- package/domain/schemas/input_output.go
+++ package/domain/schemas/input_output.go
@@ -1,9 +1,19 @@
 package schemas
 
 type InputOutput struct {
-	ID          int64   `json:"-"`
-	TaskID      int64   `json:"task_id"`
-	Order       int     `json:"order"`
-	TimeLimit   float64 `json:"time_limit"`
-	MemoryLimit float64 `json:"memory_limit"`
+	ID          int64 `json:"-"`
+	TaskID      int64 `json:"-"`
+	Order       int   `json:"order"`
+	TimeLimit   int64 `json:"time_limit"`
+	MemoryLimit int64 `json:"memory_limit"`
+}
+
+type PutInputOutputRequest struct {
+	Limits []PutInputOutput `json:"limits"`
+}
+
+type PutInputOutput struct {
+	Order       int   `json:"order" validate:"gt=0"`
+	TimeLimit   int64 `json:"time_limit" validate:"gt=0"`
+	MemoryLimit int64 `json:"memory_limit" validate:"gt=0"`
 }
--- package/repository/input_output_repository.go
+++ package/repository/input_output_repository.go
@@ -11,7 +11,13 @@ type InputOutputRepository interface {
 	// DeleteAll deletes all input output records for given task
 	DeleteAll(tx *gorm.DB, taskID int64) error
 	// GetInputOutputID returns the ID of the input output record with the given task ID and order
-	GetInputOutputID(db *gorm.DB, taskID int64, order int64) (int64, error)
+	GetInputOutputID(db *gorm.DB, taskID int64, order int) (int64, error)
+	// GetByTask returns all input/output for given task
+	GetByTask(db *gorm.DB, taskID int64) ([]models.InputOutput, error)
+	// Get returns input/ouput with given ID
+	Get(tx *gorm.DB, ioID int64) (*models.InputOutput, error)
+	// Put updates input/output with given ID
+	Put(tx *gorm.DB, inputOutput *models.InputOutput) error
 }
 
 type inputOutputRepository struct{}
@@ -21,7 +27,7 @@ func (i *inputOutputRepository) Create(tx *gorm.DB, inputOutput *models.InputOut
 	return err
 }
 
-func (i *inputOutputRepository) GetInputOutputID(tx *gorm.DB, taskID int64, order int64) (int64, error) {
+func (i *inputOutputRepository) GetInputOutputID(tx *gorm.DB, taskID int64, order int) (int64, error) {
 	var inputOutputID int64
 	err := tx.Table("input_outputs").Select("id").Where(
 		`task_id = ? AND "order" = ?`,
@@ -33,6 +39,31 @@ func (i *inputOutputRepository) GetInputOutputID(tx *gorm.DB, taskID int64, orde
 
 func (i *inputOutputRepository) DeleteAll(tx *gorm.DB, taskID int64) error {
 	err := tx.Where("task_id = ?", taskID).Delete(&models.InputOutput{}).Error
+	return err
+}
+
+func (i *inputOutputRepository) GetByTask(tx *gorm.DB, taskID int64) ([]models.InputOutput, error) {
+	inputOutput := []models.InputOutput{}
+	err := tx.Model(&models.InputOutput{}).Where("task_id = ?", taskID).Find(&inputOutput).Error
+	if err != nil {
+		return nil, err
+	}
+
+	return inputOutput, nil
+}
+
+func (i *inputOutputRepository) Get(tx *gorm.DB, ioID int64) (*models.InputOutput, error) {
+	inputOutput := &models.InputOutput{}
+	err := tx.Model(&models.InputOutput{}).Where("id = ?", ioID).First(inputOutput).Error
+	if err != nil {
+		return nil, err
+	}
+
+	return inputOutput, nil
+}
+
+func (i *inputOutputRepository) Put(tx *gorm.DB, inputOutput *models.InputOutput) error {
+	err := tx.Save(inputOutput).Error
 	return err
 }
 
