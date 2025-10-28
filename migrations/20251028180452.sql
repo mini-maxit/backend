165b7d4cab83880a3caaf65e7d6506f6619e03b3
HermanPlay <hermanplay1110@gmail.com>
2025-10-28 23:43:52 +0100

    chore: merge migrations

--- migrations/20251028180452.sql
+++
@@ -1,2 +0,0 @@
--- Modify "submissions" table
-ALTER TABLE "maxit"."submissions" ADD COLUMN "contest_id" bigint NULL, ADD CONSTRAINT "fk_maxit_submissions_contest" FOREIGN KEY ("contest_id") REFERENCES "maxit"."contests" ("id") ON UPDATE NO ACTION ON DELETE NO ACTION;
